e1a5416190a9378c8b0025e434115b92a3386217
Chris Weiss <bitblit@gmail.com>
2014-09-15 19:26:03 -0700

    Adding

--- hpglServe.py
+++ hpglServe.py
@@ -3,6 +3,10 @@ import serial
 f = open('knownGood.hpgl', 'w')
 print f
 
+hp = f.read();
+
+print hp
+
 ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=1)
 x = ser.read()          # read one byte
 s = ser.read(10)        # read up to ten bytes (timeout)

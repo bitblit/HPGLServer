7c7252aa83e7bb3c9273b418699baecc9bc121d7
Chris Weiss <bitblit@gmail.com>
2014-09-15 20:12:06 -0700

    Adding

--- hpglServe.py
+++ hpglServe.py
@@ -1,6 +1,6 @@
 import serial
 
-with open('/home/pi/workspace/HPGLServer/knownGood.hpgl', 'r') as f:
+with open('knownGood.hpgl', 'r') as f:
     hp = f.read()
 f.closed
 
@@ -8,7 +8,11 @@ print 'Before'
 print hp
 print 'After'
 
-ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=1)
+#ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1, xonxoff=False, rtscts=False, writeTimeout=None, dsrdtr=False, interCharTimeout=None)
+#ser = serial.Serial(ort=None, baudrate=9600, bytesize=EIGHTBITS, parity=PARITY_NONE, stopbits=STOPBITS_ONE, timeout=None, xonxoff=False, rtscts=False, writeTimeout=None, dsrdtr=False, interCharTimeout=None)
+#ser = serial.Serial(0)  # open first serial port
+print ser.name          # check which port was really used
 ser.write(hp)
 #x = ser.read()          # read one byte
 #s = ser.read(10)        # read up to ten bytes (timeout)
